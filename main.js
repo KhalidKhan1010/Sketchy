5445ff0a970d484cb7f6ec699c2c5fd903c0fa17
Khalid Khan <khalidkhankhan1010@gmail.com>
2023-01-20 19:06:22 +0500

    Add option: Dynamically resize the canvas Add option to change the no. of divs in the canvas grid (according to user-provided value). Change the no. of divs ONLY. Width & height of the canvas in pixels remains unchanged.

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+let canvasSizePixels = document.getElementById('container-one-canvas').style.width;
+
+function createCanvasContainer(){
+    const container = document.createElement('div');
+    container.setAttribute('id', 'container-two-canvas');
+    container.style.width = canvasSizePixels;
+    container.style.height = canvasSizePixels;
+    document.getElementById('container-one-canvas').append(container);
+    //container.style.border = '0.5px solid black';
+}
+
 function drawCanvas(canvasSize){
-    let canvas = document.getElementById('container-one-canvas');
+    let canvas = document.getElementById('container-two-canvas');
     let flexboxesArray = [];
 
-    let canvasBlockDivWidth = Number(canvas.style.width.slice(0, -2)) / canvasSize;
-    let canvasBlockDivHeight = Number(canvas.style.height.slice(0, -2)) / canvasSize;
+    let canvasBlockDivWidth = canvas.offsetWidth / canvasSize;
+    let canvasBlockDivHeight = canvas.offsetHeight / canvasSize;
+
+    /* let canvasBlockDivWidth = Number(canvas.style.width.slice(0, -2)) / canvasSize;
+    let canvasBlockDivHeight = Number(canvas.style.height.slice(0, -2)) / canvasSize; */
 
     for(let i = 0; i < canvasSize; ++i)
     {
@@ -24,4 +38,15 @@ function drawCanvas(canvasSize){
     }
 }
 
-drawCanvas(16);
+function eraseCanvas(){
+    document.getElementById('container-two-canvas').remove();
+}
+
+createCanvasContainer();
+drawCanvas(Number(document.getElementById('canvas-size').value));
+
+document.getElementById('draw-canvas').addEventListener('click', function(){
+    eraseCanvas();
+    createCanvasContainer();
+    drawCanvas(Number(document.getElementById('canvas-size').value));
+});
